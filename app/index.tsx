ba25919c3432d8a9a44c8d1b12828d848f580c8b
Gal Polak <gal.polak@gmail.com>
2024-10-05 12:38:18 +0300

    update app and fonts

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-      <Text className="text-green-600">Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-green-600 font-pextrabold">Test.</Text>
     </View>
   );
 }
